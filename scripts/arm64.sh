dfa7e25f0ba01185563549ce865d6c224dd81b0f
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-03-30 16:47:22 +0700

    Update arm64.sh

--- scripts/arm64.sh
+++ scripts/arm64.sh
@@ -2,7 +2,9 @@
 rm -rf kernel
 git clone $REPO -b $BRANCH kernel 
 cd kernel
-curl -LSs "https://raw.githubusercontent.com/ThRE-Team/KernelSU-Next/refs/heads/next-time/kernel/setup.sh" | bash -s next-time
+git submodule add https://github.com/rifsxd/KernelSU-Next
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s next
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
 echo "Nuke previous toolchains"
 rm -rf toolchain out AnyKernel
 echo "cleaned up"
