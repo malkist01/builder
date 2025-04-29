05e93c6159c04fa9d8a8f707f1495a7433be347f
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-04-30 03:24:29 +0700

    Update A7.sh

--- scripts/A7.sh
+++ scripts/A7.sh
@@ -53,7 +53,7 @@ function compile() {
 # Zipping
 zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Teletubies-"${CODENAME}"-Arm64"${DATE}".zip ./*
+    zip -r9 Teletubies-"${CODENAME}"-Arm64-"${DATE}".zip ./*
     cd ..
 }
 compile
