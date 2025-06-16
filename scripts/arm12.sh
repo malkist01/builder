2e354ef2fa6440cc7fea713ce9759f8989739852
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-06-17 05:28:01 +0700

    Update arm12.sh

--- scripts/arm12.sh
+++ scripts/arm12.sh
@@ -51,7 +51,7 @@ function compile() {
 # Zipping
 zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Teletubies-A11-"${CODENAME}"-arm"${DATE}".zip ./*
+    zip -r9 Teletubies-A11-"${CODENAME}"-arm-"${DATE}".zip ./*
     cd ..
 }
 compile
