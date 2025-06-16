84cd0376817c4b935c8b9715e7d51bb14e89ede3
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-06-16 14:15:12 +0700

    Update arm12.sh

--- scripts/arm12.sh
+++ scripts/arm12.sh
@@ -38,9 +38,8 @@ function push() {
 }
 # Compile plox
 function compile() {
-     make O=$OUT_DIR ARCH=$ARCH KCFLAGS=-mno-android $DEF
-     make O=$OUT_DIR ARCH=$ARCH KCFLAGS=-mno-android -j$(nproc --all)
-
+     make -C $(pwd) O=out KCFLAGS=-mno-android ${DEF}
+     make -j64 -C $(pwd) KCFLAGS=-mno-android O=out
      if ! [ -a "$IMAGE" ]; then
         finderr
         exit 1
