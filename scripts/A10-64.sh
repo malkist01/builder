43f34b893cd2eb35ca29b754c5f2304e55ae0f8c
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-07-04 11:12:42 +0700

    Update A10-64.sh

--- scripts/A10-64.sh
+++ scripts/A10-64.sh
@@ -2,7 +2,6 @@
 rm -rf kernel
 git clone $REPO -b $BRANCH kernel 
 cd kernel
-curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
 echo "Nuke previous toolchains"
 rm -rf toolchain out AnyKernel
 echo "cleaned up"
