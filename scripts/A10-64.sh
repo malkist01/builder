7613d461c2603885d892dbce83c7fd2a22935ef6
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-07-17 16:27:18 +0700

    Update A10-64.sh

--- scripts/A10-64.sh
+++ scripts/A10-64.sh
@@ -2,6 +2,7 @@
 rm -rf kernel
 git clone $REPO -b $BRANCH kernel 
 cd kernel
+curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s main
 echo "Nuke previous toolchains"
 rm -rf toolchain out AnyKernel
 echo "cleaned up"
