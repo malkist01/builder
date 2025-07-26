1d3f3d3ec6e36fb51869fca48724c01ab7a89885
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-07-26 18:05:48 +0700

    Update mido.sh

--- scripts/mido.sh
+++ scripts/mido.sh
@@ -7,7 +7,7 @@ cd kernel
 
 rm -rf KernelSU
 
-curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-main
+curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
 
 clang() {
     rm -rf clang
