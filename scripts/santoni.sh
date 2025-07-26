77927c63d94bcc9a5d54e3611e661df24217e3d5
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-07-27 03:57:38 +0700

    Update santoni.sh

--- scripts/santoni.sh
+++ scripts/santoni.sh
@@ -7,7 +7,7 @@ cd kernel
 
 rm -rf KernelSU
 
-curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
+curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-main
 
 clang() {
     rm -rf clang
