311dea1181cfeebe6a9fc11eb28a5650d7724bc9
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-07-26 18:14:33 +0700

    Create santoni.sh

---
+++ scripts/santoni.sh
@@ -0,0 +1,158 @@
+#!/usr/bin/env bash
+
+# Dependencies
+rm -rf kernel
+git clone $REPO -b $BRANCH kernel 
+cd kernel
+
+rm -rf KernelSU
+
+curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
+
+clang() {
+    rm -rf clang
+    echo "Cloning clang"
+    if [ ! -d "clang" ]; then
+        git clone https://gitlab.com/clangsantoni/zyc_clang.git -b 21 --depth=1 clang
+        KBUILD_COMPILER_STRING="ZyC clang"
+        PATH="${PWD}/clang/bin:${PATH}"
+    fi
+    sudo apt install -y ccache
+    echo "Done"
+}
+
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+DATE=$(date +"%Y%m%d-%H%M")
+START=$(date +"%s")
+KERNEL_DIR=$(pwd)
+#Ccache
+export USE_CCACHE=1
+export CCACHE_COMPILER_CHECK="%compiler% -dumpversion"
+export CCACHE_MAXFILES="0"
+export CCACHE_NOHASHDIR="true"
+export CCACHE_UMASK="0002"
+export CCACHE_COMPRESSION="true"
+export CCACHE_COMPRESSION_LEVEL="-3"
+export CCACHE_NOINODECACHE="true"
+export CCACHE_COMPILERTYPE="auto"
+export CCACHE_RUN_SECOND_CPP="true"
+export CCACHE_SLOPPINESS="file_macro,time_macros,include_file_mtime,include_file_ctime,file_stat_matches"
+export TZ=Asia/Jakarta
+export KBUILD_COMPILER_STRING
+ARCH=arm64
+export ARCH
+KBUILD_BUILD_HOST="android"
+export KBUILD_BUILD_HOST
+KBUILD_BUILD_USER="malkist"
+export KBUILD_BUILD_USER
+DEVICE="Xiaomi Redmi 4X"
+export DEVICE
+CODENAME="santoni"
+export CODENAME
+DEFCONFIG="teletubies_defconfig"
+export DEFCONFIG
+COMMIT_HASH=$(git rev-parse --short HEAD)
+export COMMIT_HASH
+PROCS=$(nproc --all)
+export PROCS
+STATUS=STABLE
+export STATUS
+source "${HOME}"/.bashrc && source "${HOME}"/.profile
+if [ $CACHE = 1 ]; then
+    ccache -M 100G
+    export USE_CCACHE=1
+fi
+LC_ALL=C
+export LC_ALL
+
+tg() {
+    curl -sX POST https://api.telegram.org/bot"${token}"/sendMessage -d chat_id="${chat_id}" -d parse_mode=Markdown -d disable_web_page_preview=true -d text="$1" &>/dev/null
+}
+
+tgs() {
+    MD5=$(md5sum "$1" | cut -d' ' -f1)
+    curl -fsSL -X POST -F document=@"$1" https://api.telegram.org/bot"${token}"/sendDocument \
+        -F "chat_id=${chat_id}" \
+        -F "parse_mode=Markdown" \
+        -F "caption=$2 | *MD5*: \`$MD5\`"
+}
+
+# Send Build Info
+sendinfo() {
+    tg "
+• Compiler Action •
+*Building on*: \`Github actions\`
+*Date*: \`${DATE}\`
+*Device*: \`${DEVICE} (${CODENAME})\`
+*Branch*: \`$(git rev-parse --abbrev-ref HEAD)\`
+*Last Commit*: [${COMMIT_HASH}](${REPO}/commit/${COMMIT_HASH})
+*Compiler*: \`${KBUILD_COMPILER_STRING}\`
+*Build Status*: \`${STATUS}\`"
+}
+
+# Push kernel to channel
+push() {
+    cd AnyKernel || exit 1
+    ZIP=$(echo *.zip)
+    tgs "${ZIP}" "Build took $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s). | For *${DEVICE} (${CODENAME})* | ${KBUILD_COMPILER_STRING}"
+}
+
+# Catch Error
+finderr() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d sticker="CAADBQADZwADqZrmFoa87YicX2hwAg" \
+        -d text="Build throw an error(s)"
+    error_sticker
+    exit 1
+}
+
+# Compile
+compile() {
+
+    if [ -d "out" ]; then
+        rm -rf out && mkdir -p out
+    fi
+
+    make O=out ARCH="${ARCH}" "${DEFCONFIG}"
+    make -j"${PROCS}" O=out \
+        ARCH=$ARCH \
+               CC="ccache clang" \
+                     CXX="ccache clang++" \
+                            HOSTCC="ccache clang" \
+                            HOSTCXX="ccache clang++" \
+                            LD=ld.lld \
+                            AR=llvm-ar \
+                             AS=llvm-as \
+                             NM=llvm-nm \
+                             OBJCOPY=llvm-objcopy \
+                            OBJDUMP=llvm-objdump \
+                             STRIP=llvm-strip \
+         LLVM=1 \
+        CROSS_COMPILE=aarch64-linux-gnu- \
+        CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+    if ! [ -a "$IMAGE" ]; then
+        finderr
+        exit 1
+    fi
+
+    git clone --depth=1 https://github.com/malkist01/anykernel3.git AnyKernel -b master
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+}
+# Zipping
+zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 Teletubies-"${BRANCH}"-"${CODENAME}"-"${DATE}".zip ./*
+    cd ..
+}
+
+clang
+sendinfo
+compile
+zipping
+END=$(date +"%s")
+DIFF=$((END - START))
+push
