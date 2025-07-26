f311c924190beb78a832e58a5421fcaccf612eef
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-07-26 09:24:15 +0700

    Update mido.sh

--- scripts/mido.sh
+++ scripts/mido.sh
@@ -7,7 +7,13 @@ cd kernel
 
 rm -rf KernelSU
 
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
+git clone --depth=1 https://gitlab.com/simonpunk/susfs4ksu.git -b kernel-4.9 susfs4ksu
+            cp susfs4ksu/kernel_patches/50_add_susfs_in_kernel-4.9.patch ./
+            cp susfs4ksu/kernel_patches/fs/* ./fs
+            cp susfs4ksu/kernel_patches/include/linux/* ./include/linux
+            patch -p1 < 50_add_susfs_in_kernel-4.9.patch
+
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s next-susfs
 
 clang() {
     rm -rf clang
