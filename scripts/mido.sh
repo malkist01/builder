7f54b666ae6d046f6ef64bb92c6896f49737b4e2
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-07-27 21:29:03 +0700

    Update mido.sh

--- scripts/mido.sh
+++ scripts/mido.sh
@@ -118,18 +118,17 @@ compile() {
 
     make O=out ARCH="${ARCH}" "${DEFCONFIG}"
     make -j"${PROCS}" O=out \
-        ARCH=$ARCH \
-               CC="ccache clang" \
-                     CXX="ccache clang++" \
-                            HOSTCC="ccache clang" \
-                            HOSTCXX="ccache clang++" \
-                            LD=ld.lld \
-                            AR=llvm-ar \
-                             AS=llvm-as \
-                             NM=llvm-nm \
-                             OBJCOPY=llvm-objcopy \
-                            OBJDUMP=llvm-objdump \
-                             STRIP=llvm-strip \
+         ARCH=$ARCH \
+         CC="clang" \
+         CXX="clang++" \
+         HOSTCC="clang" \
+         HOSTCXX="clang++" \
+         AR=llvm-ar \
+         AS=llvm-as \
+         NM=llvm-nm \
+         OBJCOPY=llvm-objcopy \
+         OBJDUMP=llvm-objdump \
+         STRIP=llvm-strip \
          LLVM=1 \
         CROSS_COMPILE=aarch64-linux-gnu- \
         CROSS_COMPILE_ARM32=arm-linux-gnueabi-
