c3a9475a4945a12c95ba0f5c9d08c6c14ce7d786
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-08-15 12:36:22 +0700

    Update A7v2.sh

--- scripts/A7v2.sh
+++ scripts/A7v2.sh
@@ -2,7 +2,7 @@
 rm -rf kernel
 git clone $REPO -b $BRANCH kernel 
 cd kernel
-curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSUonArm32/main/kernel/setup.sh" | bash -s main
+curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
 echo "Nuke previous toolchains"
 rm -rf toolchain out AnyKernel
 echo "cleaned up"
