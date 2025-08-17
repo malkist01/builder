0ab4e53c980eccf2e52fd3e0995ea850a8332c03
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-08-17 22:44:21 +0700

    Create j4primelte.sh

---
+++ scripts/j4primelte.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+rm -rf kernel
+git clone $REPO -b $BRANCH kernel 
+cd kernel
+echo "Nuke previous toolchains"
+rm -rf toolchain out AnyKernel
+echo "cleaned up"
+echo "Cloning toolchain"
+git clone --depth=1 https://github.com/KudProject/arm-linux-androideabi-4.9.git -b master gcc32
+if [ "$is_test" = true ]; then
+     echo "Its alpha test build"
+     unset chat_id
+     unset token
+     export chat_id=${my_id}
+     export token=${nToken}
+else
+     echo "Its beta release build"
+fi
+SHA=$(echo $DRONE_COMMIT_SHA | cut -c 1-8)
+IMAGE=$(pwd)/out/arch/arm/boot/zImage
+DATE=$(date +'%H%M-%d%m%y')
+START=$(date +"%s")
+CODENAME=j4primelte
+DEF=j4primelte_defconfig
+export CROSS_COMPILE="$(pwd)/gcc32/bin/arm-linux-androideabi-"
+export PATH="$(pwd)/gcc32/bin:$PATH"
+export ARCH=arm
+export KBUILD_BUILD_USER=malkist
+export KBUILD_BUILD_HOST=android
+# Push kernel to channel
+function push() {
+    cd AnyKernel || exit 1
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
+        -F chat_id="$chat_id" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Samsung J6+</b>"
+}
+# Compile plox
+function compile() {
+     make -C $(pwd) O=out ${DEF}
+     make -j64 -C $(pwd) O=out
+     if ! [ -a "$IMAGE" ]; then
+        finderr
+        exit 1
+     fi
+    git clone --depth=1 https://github.com/malkist01/anykernel3.git AnyKernel -b master
+    cp out/arch/arm/boot/zImage AnyKernel
+}
+# Zipping
+zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 Teletubies-j4primelte-"${CODENAME}"-arm-"${DATE}".zip ./*
+    cd ..
+}
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
