ebfd54350ed2befde286bfed1f7e4ba397b0e943
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-08-23 17:07:49 +0700

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,6 +6,8 @@ git clone $REPO -b $BRANCH kernel
 cd kernel
 rm -rf KernelSU
 
+git fetch https://github.com/KanonifyX/android_kernel_sony_tama/commit && git cherry-pick 2a2b02b9ee1a64c426bcb5a4452caeb08e63f81b
+
 # integrate sukisu-ultra
 curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
 
