6de412c9e85cd6929137ac7311b6540bb4f6fb10
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-10 07:34:17 +0700

    Update mido.sh

--- scripts/mido.sh
+++ scripts/mido.sh
@@ -7,6 +7,12 @@ cd kernel
 
 rm -rf KernelSU
 
+git clone --depth=1 https://gitlab.com/simonpunk/susfs4ksu.git -b kernel-4.9 susfs4ksu
+            cp susfs4ksu/kernel_patches/50_add_susfs_in_kernel-4.9.patch ./
+            cp susfs4ksu/kernel_patches/fs/* ./fs
+            cp susfs4ksu/kernel_patches/include/linux/* ./include/linux
+            patch -p1 < 50_add_susfs_in_kernel-4.9.patch
+
 git fetch https://github.com/KanonifyX/android_kernel_tama_sdm845-old ksu
 
 git cherry-pick 35a5b1ae05ac28ed4496fa612fd5459925e6407c
