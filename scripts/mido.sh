0d5704b95ebe2bb5d31c4aeae7e11295b494bce2
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-13 22:34:09 +0700

    Update mido.sh

--- scripts/mido.sh
+++ scripts/mido.sh
@@ -11,7 +11,7 @@ chmod +x patch.sh
 ./patch.sh
 
 # Add KernelSU
-curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
+curl -LSs "https://raw.githubusercontent.com/WildKernels/Wild_KSU/wild/kernel/setup.sh" | bash -s wild
 
 #add KSU Config
 echo "Adding CONFIG_KSU.."
