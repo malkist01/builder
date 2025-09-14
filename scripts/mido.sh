8b2bd07320457d89f82084a1d1865332e8a8cd99
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-14 23:45:01 +0700

    Update mido.sh

--- scripts/mido.sh
+++ scripts/mido.sh
@@ -6,9 +6,11 @@ git clone $REPO -b $BRANCH kernel
 cd kernel
 rm -rf KernelSU
 
-chmod +x add_susfs.patch && susfs_ksu.patch
-patch -p1 < add_susfs.patch && susfs_ksu.patch
-
+git clone --depth=1 https://gitlab.com/simonpunk/susfs4ksu.git -b kernel-4.9 susfs4ksu
+cp susfs4ksu/kernel_patches/50_add_susfs_in_kernel-4.9.patch ./
+cp susfs4ksu/kernel_patches/fs/* ./fs
+cp susfs4ksu/kernel_patches/include/linux/* ./include/linux
+patch -p1 < 50_add_susfs_in_kernel-4.9.patch
 # Add KernelSU
 curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s next-susfs
 
