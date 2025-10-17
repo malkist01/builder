e60972c2e18f3a0fcc69c85f47c7c8876635f8f4
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-10-17 17:45:09 +0700

    Update arm64.sh

--- scripts/arm64.sh
+++ scripts/arm64.sh
@@ -3,7 +3,6 @@ rm -rf kernel
 git clone $REPO -b $BRANCH kernel 
 cd kernel
 curl -LSs "https://raw.githubusercontent.com/malkist01/patch/main/add/patch.sh" | bash -s main
-curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
 echo "Nuke previous toolchains"
 rm -rf toolchain out AnyKernel
 echo "cleaned up"
