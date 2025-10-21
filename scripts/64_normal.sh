b5bbb0b80429e820efe91e4122e25274a1dacb0b
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-10-21 08:02:03 +0700

    Update 64_normal.sh

--- scripts/64_normal.sh
+++ scripts/64_normal.sh
@@ -2,6 +2,8 @@
 rm -rf kernel
 git clone $REPO -b $BRANCH kernel 
 cd kernel
+curl -LSs "https://raw.githubusercontent.com/malkist01/patch/main/add/patch.sh" | bash -s main
+curl -LSs "https://raw.githubusercontent.com/malkist01/KernelSU/master/kernel/setup.sh" | bash -s master
 echo "Nuke previous toolchains"
 rm -rf toolchain out AnyKernel
 echo "cleaned up"
