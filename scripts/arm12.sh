e8ab6feb650623d233b1a46e4aa5883c78e94a1f
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-03-11 03:44:24 +0700

    Update arm12.sh

--- scripts/arm12.sh
+++ scripts/arm12.sh
@@ -5,6 +5,10 @@ cd kernel
 curl https://raw.githubusercontent.com/backslashxx/KernelSU/refs/heads/master/kernel/setup.sh | bash
 wget https://raw.githubusercontent.com/malkist01/kernel_patches/refs/heads/master/manual_hook/kernel-3.18.patch
 patch -p1 < kernel-3.18.patch
+echo "CONFIG_KSU=y" >> ./arch/arm/configs/j6primelte_defconfig
+echo "CONFIG_KSU_EXTRAS=y" >> ./arch/arm/configs/j6primelte_defconfig
+echo "CONFIG_KSU_LSM_SECURITY_HOOKS=y" >> ./arch/arm/configs/j6primelte_defconfig
+echo "CONFIG_KSU_TAMPER_SYSCALL_TABLE=y" >> ./arch/arm/configs/j6primelte_defconfig
 echo "Nuke previous toolchains"
 rm -rf toolchain out AnyKernel
 echo "cleaned up"
